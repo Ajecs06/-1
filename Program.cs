dc395bfb548d50c6e458bbcde525b80db48d1d83
Ajecs06 <Ajecs06@mail.ru>
2025-04-07 14:00:36 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ZAD_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Кто ");//это что то
+            string Имя = Console.ReadLine();
+            Console.WriteLine($"кто {Имя}");//это
+        }
+    }
+}
